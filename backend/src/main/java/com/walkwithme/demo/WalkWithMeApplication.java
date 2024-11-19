66b7d8ba5934990dfee25fa238081f7d10cc5cf0
Kafi Rashid <kafi.rashid@gmail.com>
2024-11-18 18:12:32 -0600

    Init

---
+++ backend/src/main/java/com/walkwithme/demo/WalkWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.walkwithme.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkWithMeApplication.class, args);
+	}
+
+}
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function Home(): React.JSX.Element {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
