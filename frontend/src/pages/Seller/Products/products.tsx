d71a06e27d22723ab7e181f0afa9f9c71fe8e948
Kafi Rashid <kafi.rashid@gmail.com>
2024-11-22 18:03:53 -0600

    Filtered seller specific prods

--- frontend/src/pages/Seller/Products/products.tsx
+++ frontend/src/pages/Seller/Products/products.tsx
@@ -34,24 +34,27 @@ export default function Products(): React.JSX.Element {
   const [userObj, setUserObj] = React.useState('');
   const [userProfile, setUserProfile] = React.useState<any>({});
   const navigate = useNavigate();
-  const { user, setUser } = useContext(UserContext);
   const [canAddProduct, setCanAddProduct] = React.useState(false);
   const axios = useAxios();
 
   React.useEffect(() => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      setUserObj(JSON.parse(user));
+    }
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
     axios.get('/products')
       .then(({ data }) => {
-        setProducts(data);
+        const prods = data?.filter(d => d?.seller?.id === userObj?.userId);
+        setProducts(prods);
       })
       .catch((error) => {
         console.log("Error", error);
       });
-
-      const user = localStorage.getItem('user');
-    if (user) {
-      setUserObj(JSON.parse(user));
-    }
-  }, []);
+  }
 
   React.useEffect(() => {
     if (userObj) {
