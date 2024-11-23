f6f2150281551a1e63f06c07887a0ffb483451b7
Kafi Rashid <kafi.rashid@gmail.com>
2024-11-22 20:05:29 -0600

    Small UI changes

--- frontend/src/pages/Admin/Config/Brands/brands.tsx
+++ frontend/src/pages/Admin/Config/Brands/brands.tsx
@@ -214,9 +214,6 @@ export default function Brands(): React.JSX.Element {
                     <Icon name='chevron left' />
                   </MenuItem>
                   <MenuItem as='a'>1</MenuItem>
-                  <MenuItem as='a'>2</MenuItem>
-                  <MenuItem as='a'>3</MenuItem>
-                  <MenuItem as='a'>4</MenuItem>
                   <MenuItem as='a' icon>
                     <Icon name='chevron right' />
                   </MenuItem>
--- frontend/src/pages/Admin/Config/SubCategories/subcategories.tsx
+++ frontend/src/pages/Admin/Config/SubCategories/subcategories.tsx
@@ -205,9 +205,6 @@ export default function SubCategories(): React.JSX.Element {
                     <Icon name='chevron left' />
                   </MenuItem>
                   <MenuItem as='a'>1</MenuItem>
-                  <MenuItem as='a'>2</MenuItem>
-                  <MenuItem as='a'>3</MenuItem>
-                  <MenuItem as='a'>4</MenuItem>
                   <MenuItem as='a' icon>
                     <Icon name='chevron right' />
                   </MenuItem>
